e3491bb1a524bb56904e7b148141207290ada1ba
Denis Kiselev <k1selevde@yandex.ru>
2020-09-20 13:06:30 +0500

    add search types

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -23,3 +23,59 @@ export type collectionMovie = {
     overview: string,
     release_date: string
 }
+
+// search results types:
+
+export type searchResultMovie = {
+    vote_count: number,
+    popularity: number,
+    id: number,
+    video: boolean,
+    media_type: "movie",
+    vote_average: number,
+    title: string,
+    release_date: string,
+    original_language: string,
+    original_title: string,
+    genre_ids: number[],
+    backdrop_path: string,
+    adult: boolean,
+    overview: string,
+    poster_path: string
+}
+
+export type searchResultTv = {
+    original_name: string,
+    genre_ids: number[],
+    media_type: "tv",
+    name: string,
+    popularity: number,
+    origin_country: string[],
+    vote_count: number,
+    first_air_date: string,
+    backdrop_path: string,
+    original_language: string,
+    id: number,
+    vote_average: number,
+    overview: string,
+    poster_path: string
+}
+
+export type searchResultPerson = {
+    known_for_department: string,
+    id: number,
+    name: string,
+    known_for: any[],
+    popularity: number,
+    profile_path: string,
+    gender: number,
+    media_type: "person",
+    adult: boolean
+}
+
+export type searchResultCompany = {
+    id: number,
+    logo_path: null | string,
+    name: string,
+    origin_country: string
+}
